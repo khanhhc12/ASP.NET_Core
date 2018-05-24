76666bc551821fd8ee7e49ca8de2ce197a7b3b89
khanhhc12 <khanhhc12@users.noreply.github.com>
2018-05-24 18:05:19 +0700

    Update SimpleHttpClient.cs

--- HelloWorld/SimpleHttpClient.cs
+++ HelloWorld/SimpleHttpClient.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,15 +43,86 @@ namespace HelloWorld
         {
             try
             {
+                var content = new StringContent(data == null ? string.Empty : data, Encoding.UTF8, contentType);
                 using (var httpClient = new HttpClient())
                 // deadlock ConfigureAwait(false)
-                using (var response = await httpClient.PostAsync(
-                    uri,
-                    new StringContent(data == null ? string.Empty : data, Encoding.UTF8, contentType)
-                    ).ConfigureAwait(false))
+                using (var response = await httpClient.PostAsync(uri, content).ConfigureAwait(false))
                 {
                     response.EnsureSuccessStatusCode();
                     return await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (HttpRequestException hre)
+            {
+                string message = hre.Message;
+                if (hre.InnerException != null && !string.IsNullOrEmpty(hre.InnerException.Message))
+                    message += " >> " + hre.InnerException.Message;
+                return message;
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                    message += " >> " + ex.InnerException.Message;
+                return message;
+            }
+        }
+
+        public static async Task<string> GetCookieAsync(string uri, string action, List<Cookie> cookies)
+        {
+            try
+            {
+                var baseAddress = new Uri(uri);
+                var cookieContainer = new CookieContainer();
+                using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+                using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
+                {
+                    foreach (var cookie in cookies)
+                        cookieContainer.Add(baseAddress, cookie);
+                    // deadlock ConfigureAwait(false)
+                    using (var response = await client.GetAsync(action).ConfigureAwait(false))
+                    {
+                        // will throw an exception if not successful
+                        response.EnsureSuccessStatusCode();
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch (HttpRequestException hre)
+            {
+                string message = hre.Message;
+                if (hre.InnerException != null && !string.IsNullOrEmpty(hre.InnerException.Message))
+                    message += " >> " + hre.InnerException.Message;
+                return message;
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                    message += " >> " + ex.InnerException.Message;
+                return message;
+            }
+        }
+
+        public static async Task<string> PostCookieAsync(string uri, string action, List<Cookie> cookies, string data, string contentType = "application/x-www-form-urlencoded")
+        {
+            try
+            {
+                var baseAddress = new Uri(uri);
+                var cookieContainer = new CookieContainer();
+                var content = new StringContent(data == null ? string.Empty : data, Encoding.UTF8, contentType);
+                using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+                using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
+                {
+                    foreach (var cookie in cookies)
+                        cookieContainer.Add(baseAddress, cookie);
+                    // deadlock ConfigureAwait(false)
+                    using (var response = await client.PostAsync(action, content).ConfigureAwait(false))
+                    {
+                        // will throw an exception if not successful
+                        response.EnsureSuccessStatusCode();
+                        return await response.Content.ReadAsStringAsync();
+                    }
                 }
             }
             catch (HttpRequestException hre)
