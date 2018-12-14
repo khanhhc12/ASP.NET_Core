5b14d88fea6b417dedc0bb81614ff4a25c9ffd9e
khanhhc12 <khanhhc12@users.noreply.github.com>
2018-12-14 21:21:26 +0700

    HelloSignalR

---
+++ HelloSignalR/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloSignalR.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HelloSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public HomeController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"Home page loaded at: {DateTime.Now}");
+            return View();
+        }
+
+        [Route("notification")]
+        public IActionResult Notify()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloSignalR/NotificationHub.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace HelloSignalR
+{
+    public class NotificationHub : Hub
+    {
+    }
+}
