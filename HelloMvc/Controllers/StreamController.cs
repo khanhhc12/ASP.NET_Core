ad49fc35868bfdff3bee2de2f4bff11a28b70472
khanhhc12 <khanhhc12@users.noreply.github.com>
2020-12-17 20:27:59 +0700

    json

--- HelloMvc/Controllers/StreamController.cs
+++ HelloMvc/Controllers/StreamController.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace HelloMvc.Controllers
 {
@@ -12,8 +11,8 @@ namespace HelloMvc.Controllers
         public IActionResult Index()
         {
             return View();
-        } 
-        
+        }
+
         [HttpGet]
         public void Test()
         {
@@ -24,14 +23,7 @@ namespace HelloMvc.Controllers
             Response.ContentType = "application/json";
             using (Response.Body)
             {
-                JsonSerializer serializer = new JsonSerializer();
-                using (var sw = new StreamWriter(Response.Body))
-                using (JsonTextWriter writer = new JsonTextWriter(sw))
-                {
-                    JToken obj = JToken.FromObject(itemToSerialize, serializer);
-                    obj.WriteTo(writer);
-                    writer.Flush();
-                }
+                JsonSerializer.SerializeAsync(Response.Body, itemToSerialize);
             }
         }
     }
